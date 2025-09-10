eb58e86ec2014eaddbb9cf4c1c633c641eae5964
Michael The <72528@fif.co.id>
2025-09-10 09:53:51 +0700

    OrderEndpoint: part-solved

--- src/main/java/com/example/boilerplate_java_springboot/dto/order/CreateOrderRequest.java
+++ src/main/java/com/example/boilerplate_java_springboot/dto/order/CreateOrderRequest.java
@@ -1,5 +1,7 @@
 package com.example.boilerplate_java_springboot.dto.order;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -13,12 +15,23 @@ import lombok.NoArgsConstructor;
 public class CreateOrderRequest {
 
     @NotNull
+    @JsonProperty("event_id")
     private int eventID;
 
     @NotBlank
-    private String username;
+    @JsonProperty("name_order")
+    private String nameOrder;
 
     @NotBlank
     @Email
-    private String email;
+    @JsonProperty("email_order")
+    private String emailOrder;
+
+    @NotBlank
+    private String orderStatus;
+
+    @DecimalMin("0.00")
+    @JsonProperty("total_amount")
+    private double totalAmount;
+
 }
--- src/main/java/com/example/boilerplate_java_springboot/entity/OrderEntity.java
+++ src/main/java/com/example/boilerplate_java_springboot/entity/OrderEntity.java
@@ -1,10 +1,15 @@
 package com.example.boilerplate_java_springboot.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jdk.jfr.Event;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Data
@@ -16,8 +21,20 @@ public class OrderEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private int eventID;
+    private String nameOrder;
+    private String emailOrder;
 
-    private String username;
-    private String email;
+    private double totalAmount;
+
+    @Enumerated(EnumType.STRING)
+    private String orderStatus;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private LocalDateTime deletedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private EventEntity eventEntity;
+
 }
